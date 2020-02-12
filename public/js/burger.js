21173659cbb710df746456f87db19be0b67fd2d5
rmtar <picpoint@yandex.ru>
2020-02-12 22:28:30 +0300

    make burgermenu

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,12 +1,14 @@
 let burgermenu = document.querySelector('.smart__burgermenu');
 let menublock = document.querySelector('.smart__menublock');
-console.log(menublock);
+let menublockfloat = document.querySelector('.smart__menublockfloat');
+console.log(menublockfloat);
 
 
 burgermenu.addEventListener('click', () => {  
   burgermenu.firstElementChild.classList.toggle('smart__burgerlineonerotate');
   burgermenu.firstElementChild.nextElementSibling.classList.toggle('smart__burgerlinetworotate');
   burgermenu.lastElementChild.classList.toggle('smart__burgerlinethreerotate');  
+  menublockfloat.classList.toggle('smart__menublockfloatshow');
   
 });
 
