e607040389f75c366ce3f50c11fc256933517a38
picpoint <picpoint@yandex.ru>
2020-02-25 00:14:38 +0300

    start make slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -14,8 +14,16 @@ class Slider {
     console.log(this.childrens);
   }
 
+  scrolling() {
+    for(let i = 0; i < this.childrens.length; i++) {
+      this.childrens[i].style.position = 'relative';
+      this.childrens[i].style.right = '100px';      
+    }
+  }
+
 }
 
 
 let block = new Slider(movies, moviesChilds);
 block.shows();
+block.scrolling();
