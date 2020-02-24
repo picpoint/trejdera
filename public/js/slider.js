eade7d0ca49f06d971722b551b2275013121b09d
picpoint <picpoint@yandex.ru>
2020-02-24 17:02:58 +0300

    start make slider

---
+++ public/js/slider.js
@@ -0,0 +1,21 @@
+let movies = document.querySelector('.promoann__movies');
+let moviesChilds = movies.children;
+
+
+
+class Slider {
+  constructor(paretnblock, childrens) {
+    this.paretnblock = paretnblock;
+    this.childrens = childrens;
+  }
+
+  shows() {
+    console.log(this.paretnblock);
+    console.log(this.childrens);
+  }
+
+}
+
+
+let block = new Slider(movies, moviesChilds);
+block.shows();
