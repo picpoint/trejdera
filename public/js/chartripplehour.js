ecde73301dfc1e52ac090172345d621d3b231e90
picpoint <picpoint@yandex.ru>
2020-03-10 22:38:56 +0300

    add json file with datas

--- public/js/chartripplehour.js
+++ public/js/chartripplehour.js
@@ -1,21 +1,7 @@
-
-// let rippHourlabels = ['3', '6', '9', '12', '15', '18', '24'];
-// let rippHourlabelName = 'ripple chart';
-// let bgColorGreen = 'green';
-// let brdColorGreen = 'green';
-// let rippHourdata = [10, 5, 15, 0, 5, 45, 50];
-
-
-// let rippchart = new Charts(rippHourlabels, rippHourlabelName, bgColorGreen, brdColorGreen, rippHourdata);
-// rippchart.chartMethod();
-
 var ctx = document.getElementById('ripplechart').getContext('2d');
-
 let rippHourlabels = ['3', '6', '9', '12', '15', '18', '24'];
 let rippHourlabelName = 'bitcoin chart';
 let rippHourdata = [10, 5, 15, 0, 5, 45, 50];
 
-
 let rippchart = new Charts(rippHourlabels, rippHourlabelName, bgColorGreen, brdColorGreen, rippHourdata);
 rippchart.chartMethod();
-
