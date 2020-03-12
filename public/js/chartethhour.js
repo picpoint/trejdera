78d74b1871da22743a251709b2a7436637583347
picpoint <picpoint@yandex.ru>
2020-03-12 16:47:06 +0300

    work with letters

--- public/js/chartethhour.js
+++ public/js/chartethhour.js
@@ -1,8 +1,8 @@
 var ctx = document.getElementById('ethereumchart').getContext('2d');
 let ethHourlabels = ['3', '6', '9', '12', '15', '18', '24'];
 let ethHourlabelName = 'ethereum chart';
-let bgColorGreen = 'green';
-let brdColorGreen = 'green';
+bgColorGreen = 'green';
+brdColorGreen = 'green';
 let ethHourdata = [40, 20, 30, 10, 50, 40, 45, 25, 40, 37, 50];
 
 let ethchart = new Charts(ethHourlabels, ethHourlabelName, bgColorGreen, brdColorGreen, ethHourdata);
--- src/js/chartcardanohour.js
+++ src/js/chartcardanohour.js
@@ -1,6 +1,8 @@
 var ctx = document.getElementById('cardanochart').getContext('2d');
 let crdHourlabels = ['3', '6', '9', '12', '15', '18', '24'];
 let crdHourlabelName = 'cardano chart';
+bgColorGreen = 'green';
+brdColorGreen = 'green';
 let crdHourdata = [0, 10, 5, 50, 30, 15, 40];
 
 let crdchart = new Charts(crdHourlabels, crdHourlabelName, bgColorGreen, brdColorGreen, crdHourdata);
