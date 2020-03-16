2ca63a23a2a25c21d20c0c6efef01642c02b106c
picpoint <picpoint@yandex.ru>
2020-03-16 16:55:13 +0300

    work with ajax datas

--- public/js/chartbit.js
+++ public/js/chartbit.js
@@ -12,60 +12,60 @@ btccharthour.chartMethod();
 
 
 
-let crdbithour = document.querySelector('.crdbithour');
+// let crdbithour = document.querySelector('.crdbithour');
 
-crdbithour.addEventListener('click', ()=> {  
-  let btcHourlabels = ['3', '6', '9', '12', '15', '18', '24'];
-  let btcHourlabelName = 'bitcoin chart';
-  bgColorRedChartHour = 'red';
-  brdColorRedChartHour = 'red';
-  let btcHourdata = [15, 0, 20, 15, 40, 10, 50, 30, 50, 40];
+// crdbithour.addEventListener('click', ()=> {  
+//   let btcHourlabels = ['3', '6', '9', '12', '15', '18', '24'];
+//   let btcHourlabelName = 'bitcoin chart';
+//   bgColorRedChartHour = 'red';
+//   brdColorRedChartHour = 'red';
+//   let btcHourdata = [15, 0, 20, 15, 40, 10, 50, 30, 50, 40];
 
-  let btccharthour = new Charts(btcHourlabels, btcHourlabelName, bgColorRedChartHour, brdColorRedChartHour, btcHourdata);
-  btccharthour.chartMethod();  
-});
-
-
-
-let crdbitday = document.querySelector('.crdbitday');
-
-crdbitday.addEventListener('click', () => {  
-  let btcDaylabels = ['5', '10', '15', '20', '25', '30'];
-  let btcDaylabelName = 'bitcoin chart';
-  let bgColorRedChartDay = 'red';
-  let brdColorRedChartDay = 'red';
-  let btcDaydata = [7, 10, 3, 15, 40, 23];
-
-  let btcDaychart = new Charts(btcDaylabels, btcDaylabelName, bgColorRedChartDay, brdColorRedChartDay, btcDaydata);
-  btcDaychart.chartMethod();
-});
+//   let btccharthour = new Charts(btcHourlabels, btcHourlabelName, bgColorRedChartHour, brdColorRedChartHour, btcHourdata);
+//   btccharthour.chartMethod();  
+// });
 
 
 
-let crdbitweek = document.querySelector('.crdbitweek');
+// let crdbitday = document.querySelector('.crdbitday');
 
-crdbitweek.addEventListener('click', () => {
-  let btcWeeklabels = ['1', '2', '3', '4'];
-  let btcWeeklabelName = 'bitcoin chart';
-  let bgColorRedChartWeek = 'red';
-  let brdColorRedChartWeek = 'red';
-  let btcWeekdata = [17, 48, 3, 15];
+// crdbitday.addEventListener('click', () => {  
+//   let btcDaylabels = ['5', '10', '15', '20', '25', '30'];
+//   let btcDaylabelName = 'bitcoin chart';
+//   let bgColorRedChartDay = 'red';
+//   let brdColorRedChartDay = 'red';
+//   let btcDaydata = [7, 10, 3, 15, 40, 23];
 
-  let btcWeekchart = new Charts(btcWeeklabels, btcWeeklabelName, bgColorRedChartWeek, brdColorRedChartWeek, btcWeekdata);
-  btcWeekchart.chartMethod();
-});
+//   let btcDaychart = new Charts(btcDaylabels, btcDaylabelName, bgColorRedChartDay, brdColorRedChartDay, btcDaydata);
+//   btcDaychart.chartMethod();
+// });
 
 
 
-let crdbitmonth = document.querySelector('.crdbitmonth');
+// let crdbitweek = document.querySelector('.crdbitweek');
 
-crdbitmonth.addEventListener('click', () => {
-  let btcMonthlabels = ['1', '2', '3'];
-  let btcMonthlabelName = 'bitcoin chart';
-  let bgColorRedChartMonth = 'red';
-  let brdColorRedChartMonth = 'red';
-  let btcMonthdata = [17, 48, 3, 15];
+// crdbitweek.addEventListener('click', () => {
+//   let btcWeeklabels = ['1', '2', '3', '4'];
+//   let btcWeeklabelName = 'bitcoin chart';
+//   let bgColorRedChartWeek = 'red';
+//   let brdColorRedChartWeek = 'red';
+//   let btcWeekdata = [17, 48, 3, 15];
 
-  let btcMonthchart = new Charts(btcMonthlabels, btcMonthlabelName, bgColorRedChartMonth, brdColorRedChartMonth, btcMonthdata);
-  btcMonthchart.chartMethod();
-});
+//   let btcWeekchart = new Charts(btcWeeklabels, btcWeeklabelName, bgColorRedChartWeek, brdColorRedChartWeek, btcWeekdata);
+//   btcWeekchart.chartMethod();
+// });
+
+
+
+// let crdbitmonth = document.querySelector('.crdbitmonth');
+
+// crdbitmonth.addEventListener('click', () => {
+//   let btcMonthlabels = ['1', '2', '3'];
+//   let btcMonthlabelName = 'bitcoin chart';
+//   let bgColorRedChartMonth = 'red';
+//   let brdColorRedChartMonth = 'red';
+//   let btcMonthdata = [17, 48, 3, 15];
+
+//   let btcMonthchart = new Charts(btcMonthlabels, btcMonthlabelName, bgColorRedChartMonth, brdColorRedChartMonth, btcMonthdata);
+//   btcMonthchart.chartMethod();
+// });
--- src/js/switchDateCharts.js
+++ src/js/switchDateCharts.js
@@ -1,30 +1,38 @@
-// let crdbitday = document.querySelector('#crdbitday');
+let crdbitday = document.querySelector('.crdbitday');
+let obj = {};
 
-// crdbitday.addEventListener('click', () => {
-//   let obj = {};
-//   let xhr = new XMLHttpRequest();
-//   let url = 'https://raw.githubusercontent.com/picpoint/trejdera/master/datasCharts.json';
-//   xhr.responseType = 'json';
-//   xhr.open('GET', url);
-//   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+crdbitday.addEventListener('click', () => {  
+  let xhr = new XMLHttpRequest();
+  let url = 'https://raw.githubusercontent.com/picpoint/trejdera/master/datasCharts.json';
+  xhr.responseType = 'json';
+  xhr.open('GET', url);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-//   xhr.addEventListener('readystatechange', () => {
-//     if(xhr.readyState == 4 && xhr.status == 200) {
-//       obj = xhr.response.btcday;
-//       console.log(obj);
-//     }
-//   });
+  xhr.addEventListener('readystatechange', () => {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+      obj = xhr.response.btcday;
+      console.log(obj);
+    }
+  });
 
-//   xhr.send();
+  xhr.send();
 
-//   var ctx = document.getElementById('bitcoinchart').getContext('2d');
-//   let btcDaylabels = obj.daylabels;
-//   let btcDaylabelName = obj.labelName;
-//   let bgColorRed = obj.bgColor;
-//   let brdColorRed = obj.brdColor;
-//   let btcDaydata = obj.dayData;
+	var ctx = document.getElementById('bitcoinchart').getContext('2d');
+	
+	let btcDaylabels = obj.daylabels;	
+	let btcDaylabelName = obj.labelName;
+	let bgColorRed = obj.bgColor;
+	let brdColorRed = obj.brdColor;
+	let btcDaydata = obj.dayData;
+	console.log('-----------------');
+	console.log(btcDaylabels);
+	console.log(btcDaylabelName);
+	console.log(bgColorRed);
+	console.log(brdColorRed);
+	console.log(btcDaydata);
+	console.log('-----------------');
 
-//   let btcDayChart = new Charts(btcDaylabels, btcDaylabelName, bgColorRed, brdColorRed, btcDaydata);
-//   btcDayChart.chartMethod();
+	let btcDayChart = new Charts(btcDaylabels, btcDaylabelName, bgColorRed, brdColorRed, btcDaydata);	
+	btcDayChart.chartMethod();
 
-// });
+});
